ba1ca1836c9ec4b96265fe2a1650416704cd9f7b
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2022-08-08 10:59:02 +0800

    Make Repository User

--- user/repository.go
+++ user/repository.go
@@ -1,1 +1,21 @@
 package user
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	Save(user User) (User, error) //balikannya user & error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func (r *repository) Save(user User) (User, error) {
+	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+
+}
