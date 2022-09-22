a50cede828d9294e0fb3ea1cdddacc588448835e
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2022-09-22 17:48:43 +0700

    Add Logout

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func main() {
 	userWebHandler := webHandler.NewUserHandler(userService)
 	campaignWebHandler := webHandler.NewCampaignHandler(campaignService, userService)
 	transactionWebHandler := webHandler.NewTransactionHandler(transactionService)
+	sessionWebHandler := webHandler.NewSessionHandler(userService)
 	
 	router := gin.Default()
 	router.Use(cors.Default())
@@ -103,6 +104,10 @@ func main() {
 	router.GET("/campaigns/show/:id", authAdminMiddleware(), campaignWebHandler.Show)
 
 	router.GET("/transactions", authAdminMiddleware(), transactionWebHandler.Index)
+
+	router.GET("/login", sessionWebHandler.New)
+	router.POST("/session", sessionWebHandler.Create)
+	router.GET("/logout", sessionWebHandler.Destroy)
 
 	router.Run()
 
